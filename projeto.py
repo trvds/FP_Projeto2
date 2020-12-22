a99702b7f40d4ee434e72a46a5bb2ebe3f95b887
Tiago Vieira da Silva <71610478+trvds@users.noreply.github.com>
2020-12-22 17:58:21 +0000

    Adicionado todas as funções de baixo nível do TAD "posicao" sem comentários.

--- projeto.py
+++ projeto.py
@@ -1,6 +1,6 @@
 # TAD posicao
 
-# construtores
+# Construtores
 def cria_posicao(c, l):  # str x str -> posicao
     """
     Recebe duas cadeias de carateres correspondentes a coluna c
@@ -10,17 +10,90 @@ def cria_posicao(c, l):  # str x str -> posicao
     :param l: Linha, pode ser '1', '2' ou '3'
     :return: Posicao do tabuleiro.
     """
-    if type(c) != str or type(l) != str:
-        raise ValueError('cria posicao: argumentos invalidos')
-    if c != 'a' and c != 'b' and c != 'c':
-        raise ValueError('cria posicao: argumentos invalidos')
-    if l != '1' and l != '2' and l != '3':
+
+    p = [c, l]
+    if not eh_posicao(p):
         raise ValueError('cria posicao: argumentos invalidos')
 
     return (c,l)
 
 
+def cria_copia_posicao(p):
+    """
+
+    :param p:
+    :return:
+    """
+    if not eh_posicao(p):
+        raise ValueError('cria posicao: argumentos invalidos')
+
+    return p
 
 
+# Seletores
+def obter_pos_c(p):
+    """
+
+    :param p:
+    :return:
+    """
+    return p[0]
 
 
+def obter_pos_l(p):
+    """
+
+    :param p:
+    :return:
+    """
+    return p[1]
+
+
+# Reconhecedores
+def eh_posicao(p):
+    """
+
+    :param p:
+    :return:
+    """
+    c = p[0]
+    l = p[1]
+
+    if type(c) != str or type(l) != str:
+        return False
+    if c != 'a' and c != 'b' and c != 'c':
+          return False
+    if l != '1' and l != '2' and l != '3':
+        return False
+
+    return True
+
+
+# Teste
+def posicoes_iguais(p1, p2):
+    """
+
+    :param p1:
+    :param p2:
+    :return:
+    """
+    if not eh_posicao(p1) or not eh_posicao(p2):
+        return False
+    if p1 != p2:
+        return False
+    return True
+
+
+# Transformador
+def posicao_para_str(p):
+    """
+
+    :param p:
+    :return:
+    """
+    return p[0]+p[1]
+
+
+# Funcoes de alto nivel
+
+def obter_posicoes_adjacentes():
