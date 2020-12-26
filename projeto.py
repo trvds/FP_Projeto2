6e17d70f42ed55cac9b68cda25bbaa559fdeffc2
Tiago Vieira da Silva <71610478+trvds@users.noreply.github.com>
2020-12-26 02:20:41 +0000

    Adicionado obter_posicoes_jogador e obter_posicoes_livres
    necessario desbuggar "obter_posicoes_livres"

--- projeto.py
+++ projeto.py
@@ -89,7 +89,7 @@ def eh_posicao(p):  # universal -> booleano
     if type(c) != int or type(l) != int:
         return False
     if c != '1' and c != '2' and c != '3':
-          return False
+        return False
     if l != '1' and l != '2' and l != '3':
         return False
 
@@ -118,7 +118,7 @@ def posicao_para_str(p):  # posicao -> str
     :param p: posicao
     :return: string com a posicao
     """
-    return obter_pos_c(p)+obter_pos_l(p)
+    return obter_pos_c(p) + obter_pos_l(p)
 
 
 # Funcoes de alto nivel--------------------------------------------------------
@@ -166,7 +166,7 @@ def cria_copia_peca(j):  # peca -> peca
 
 
 # Reconhecedor
-def eh_peca(arg):   # universal -> booleano
+def eh_peca(arg):  # universal -> booleano
     """
     Indica se determinado argumento e uma peca
     :param arg: valor da peca
@@ -179,7 +179,7 @@ def eh_peca(arg):   # universal -> booleano
 
 
 # Teste
-def pecas_iguais(j1, j2):   # peca x peca -> booleano
+def pecas_iguais(j1, j2):  # peca x peca -> booleano
     """
     Indica se duas pecas sao iguais
     :param j1: peca 1
@@ -209,7 +209,7 @@ def peca_para_str(j):  # peca -> str
 
 
 # Funcoes de alto nivel
-def peca_para_inteiro(j):   # peca -> N
+def peca_para_inteiro(j):  # peca -> N
     """
     Converte o valor de uma peca para o valor 1, -1 ou 0 dependendo da peca
     :param j: peca
@@ -248,8 +248,8 @@ def obter_peca(tab, p):  # tabuleiro x posicao -> peca
     :param p:
     :return:
     """
-    peca = tab[p[0]][p[1]]
-    return peca_para_str(peca)
+    peca = tab[p[1]][p[0]]
+    return peca
 
 
 def obter_vetor(tab, sel):  # tabuleiro x str -> tuplo de pecas
@@ -259,20 +259,22 @@ def obter_vetor(tab, sel):  # tabuleiro x str -> tuplo de pecas
     :param sel:
     :return:
     """
-    if sel == 'a':
+    if sel == '1' or sel == '2' or sel == '3':
+        sel = int(sel) - 1
+        return tuple(tab[sel])
+    elif sel == 'a':
         sel = 0
     elif sel == 'b':
         sel = 1
     elif sel == 'c':
         sel = 2
-    elif sel == '1' or sel == '2' or sel == '3':
-        return tuple(tab[int(sel)-1].copy())
     else:
         raise ValueError('obter_vetor: valores invalidos')
 
+
     tuplo = ()
     for linha in tab:
-        tuplo = tuplo + (linha[sel], )
+        tuplo = tuplo + (linha[sel],)
     return tuplo
 
 
@@ -285,7 +287,7 @@ def coloca_peca(tab, peca, pos):  # tabuleiro x peca x posicao -> tabuleiro
     :param pos:
     :return:
     """
-    tab[pos[0]][pos[1]] = peca
+    tab[pos[1]][pos[0]] = peca
     return tab
 
 
@@ -296,7 +298,7 @@ def remove_peca(tab, pos):  # tabuleiro x posicao -> tabuleiro
     :param pos:
     :return:
     """
-    tab[pos[0]][pos[1]] = 0
+    tab[pos[1]][pos[0]] = 0
     return tab
 
 
@@ -308,8 +310,8 @@ def move_peca(tab, pos1, pos2):  # tabuleiro x posicao x posicao -> tabuleiro
     :param pos2:
     :return:
     """
-    peca = tab[pos1[0]][pos1[1]]
-    tab = coloca_peca(tab, pos2, peca)
+    peca = tab[pos1[1]][pos1[0]]
+    tab = coloca_peca(tab, peca, pos2)
     tab = remove_peca(tab, pos1)
     return tab
 
@@ -345,7 +347,7 @@ def eh_posicao_livre(tab, pos):  # tabuleiro x posicao -> booleano
     :param pos:
     :return:
     """
-    if tab[pos[0]][pos[1]] == 0:
+    if tab[pos[1]][pos[0]] == 0:
         return True
     return False
 
@@ -370,18 +372,18 @@ def tabuleiro_para_str(tab):  # tabuleiro -> str
     :param tab:
     :return:
     """
-    tabstr = ''
+    count = 0
+    tabstr = '   a   b   c \n'
     for linha in range(3):
+        tabstr += str(linha + 1)+' '
         for coluna in range(3):
-            tabstr += peca_para_str(tab[coluna][linha])
+            tabstr += peca_para_str(tab[linha][coluna])
             if coluna < 2:
                 tabstr += '-'
             else:
                 tabstr += '\n'
-        if linha == 0:
-            tabstr += ' | \ | / | ' + '\n'
-        if linha == 1:
-            tabstr += ' | / | \ | ' + '\n'
+        if linha < 2:
+            tabstr += '   | \ | / | \n'
     return tabstr
 
 
@@ -396,7 +398,65 @@ def tuplo_para_tabuleiro(tab):  # tuplo -> tabuleiro
     return tabtuplo
 
 
+# Funcoes de alto nivel---------------------------------------------------------
+def obter_ganhador(tab):  # tabuleiro -> peca
+    """
+
+    :param tab:
+    :return:
+    """
+    x = cria_peca('X')
+    o = cria_peca('O')
+    livre = cria_peca(' ')
+    indices = ('1', '2', '3', 'a', 'b', 'c')
+
+    for indice in indices:
+        vetor = obter_vetor(tab, indice)
+        if vetor == (x, x, x):
+            return x
+        if vetor == (o, o, o):
+            return o
+    return livre
 
 
-tab = ([0,1,0],[1,0,0],[0,0,1])
-print(tuplo_para_tabuleiro(tab))
+def obter_posicoes_livres(tab):
+    """
+
+    :param tab:
+    :return:
+    """
+    colunas = ('a', 'b', 'c')
+    linhas = ('1', '2', '3')
+    posicoes = ()
+    for linha in linhas:
+        for coluna in colunas:
+            if pecas_iguais(obter_peca(tab, cria_posicao(coluna, linha)),
+                            cria_peca(' ')):
+                posicoes += (cria_posicao(coluna, linha),)
+    return posicoes
+
+
+def obter_posicoes_jogador(tab, peca): # tabuleiro x peca -> tuplo de posicoes
+    """
+
+    :param tab:
+    :return:
+    """
+    colunas = ('a', 'b', 'c')
+    linhas = ('1', '2', '3')
+    posicoes = ()
+    for linha in linhas:
+        for coluna in colunas:
+            if pecas_iguais(obter_peca(tab, cria_posicao(coluna, linha)), peca):
+                posicoes += (cria_posicao(coluna, linha),)
+    return posicoes
+
+# tabuleiro = ([0, 1, 0], [1, 0, 0], [0, 0, 1])
+t = cria_tabuleiro()
+tabuleiro_para_str(coloca_peca(t, cria_peca('X'), cria_posicao('a','1')))
+print(tabuleiro_para_str(t))
+print(tabuleiro_para_str(coloca_peca(t, cria_peca('O'), cria_posicao('b','2'))))
+print(tabuleiro_para_str(move_peca(t, cria_posicao('a','1'), cria_posicao('b','1'))))
+t = tuplo_para_tabuleiro(((0,1,-1),(-0,1,-1),(1,0,-1)))
+print(tabuleiro_para_str(t))
+print(tuple(posicao_para_str(p) for p in obter_posicoes_livres(t)))
